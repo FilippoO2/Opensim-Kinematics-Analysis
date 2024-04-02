4e9ee1add5719359ac5149b807ad7d872f6d35f5
Filippo <69460438+FilippoO2@users.noreply.github.com>
2024-04-02 20:25:12 +0100

    config.py

--- config.py
+++ config.py
@@ -2,29 +2,29 @@ import os
 
 # The variables you need to change:
 
-# The keypoint of interest
-keypoint = "center_of_mass"
+# The keypoint of interest e.g. ("center_of_mass")
+keypoint = 
 
-participant = "Doubles-1/P05"
-participant_mass = 82.2
-participant_age = 20
+#Name of participants folder e.g. ("P01")
+participant = 
+participant_mass =    # kg
+participant_age =     # years
 
-# The number of rows to skip in the kinematic data -  Needed if model does not start in correct position
+# The number of rows to skip in the kinematic data - Needed if model does not start in correct position 
 rows_to_skip = 35
-# The path to the csv file containing the trial and point start and end frames
-# See point_dict_creator.py for more information on csv_file setup
-csv_file = "D:/filip/Notational anaylsis updated.xlsx" 
-sheet_name = "Doubles 1" # Needed if trials are on a specific sheet
+# csv file path containing the trial point start and end frames for the participant - see point_dict_creator.py for more information on csv_file setup
+csv_file =  
+sheet_name = None # Needed if trials are on a specific sheet
 
 # The path to the folder containing the .sto files of kinematic data
-kinematics_folder = f"D:/filip/Python/Badminton Data/{participant}/trc_hrnet/kinematics" # path to directory with kinematic data
+kinematics_folder = f"......{participant}/trc_hrnet/kinematics"
 #Where you want to write the extracted metrics to
-data_path = f"D:/filip/Python/Badminton Data/{participant}"
+data_path = f"......{participant}"
 
 # Path where you want to save plots (inside P01 directory)
-plot_path = f"D:/filip/Python/Badminton Data/{participant}/plots"
+plot_path = f"......{participant}/plots"
 
 #Type of data to plot - Can be ["Position", "Velocity", "Acceleration" or "Energy"]
 plot_data = "Energy"
 
-heart_rate_folder = "D:/filip/Python/Badminton Data/Heart Rate Data" # path to directory with heart rate data
+heart_rate_folder =  # path to directory with heart rate data
